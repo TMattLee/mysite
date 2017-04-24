ae8c2a552725c7a6c939f17caeaef88b5009ca76
TMattLee <tmwill11@mail.usf.edu>
2017-04-24 20:54:39 +0000

    ds

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 $(document).ready(function(){
 	
 	$('.tab').click(function(){
-    var clicked_tab = $(this).attr('id');
-    var tab_id = '.'+ clicked_tab + '-tab';
-    $('.tab-content').addClass('hide-content');
-    $('.tab').removeClass('active');
-    $(this).addClass('active');
-    $(tab_id).removeClass('hide-content');
-
+        var clicked_tab = $(this).attr('id');
+        var tab_id = '.'+ clicked_tab + '-tab';
+        $('.tab-content').addClass('hide-content');
+        $('.tab').removeClass('active');
+        $(this).addClass('active');
+        $(tab_id).removeClass('hide-content');
+    });
     
-    
-    
-    
-  });
+    $('.frame').click(function(){
+        var url = $(this).attr('src');    
+        console.log(url);
+        var win = window.open(url, '_blank');
+        win.focus();
+    });
 
 });
